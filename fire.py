66eab75f1fa5d7d066f85734cafd30dd23f0cc8f
MYASSAR ALDAHABI <143972084+myassar22@users.noreply.github.com>
2024-03-03 01:27:56 +0300

    fire.py

--- fire.py
+++ fire.py
@@ -6,8 +6,8 @@ myy = cv2.VideoCapture(0)
 while (True):
     ret,frame = myy.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    fire = fire_myys.detectMultiScale(frame,1.2,5)                         # 1.2 دقه التعرف على النار 
-    # اذا توفرت 5 خواص للنار راح يتعرف عليها 
+    fire = fire_myys.detectMultiScale(frame,1.2,5)                         # 1.2 fire recognition accuracy 
+    # 5 the properties of fire to identify it
     for (x,y,w,h) in fire:
         roi_gray = gray[y:y+h ,  x:x+w]
         roi_color = frame[y:y+h ,  x:x+w]
@@ -15,6 +15,6 @@ while (True):
         playsound('audio.mp3')
 
     cv2.imshow('MYASSAR', frame)
-    if cv2.waitKey(1) & 0xFF == ord('m') :  # اذا فتحت الكميرا خليها شغاله لحتى اضغط حرف m 
+    if cv2.waitKey(1) & 0xFF == ord('m') :  # the latter m to break the programing  
      break
     
