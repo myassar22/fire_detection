475cdcc63367fdb5eab6405fad5db8b1d20d40d0
MYASSAR ALDAHABI <143972084+myassar22@users.noreply.github.com>
2024-03-03 01:24:38 +0300

    Add files via upload

---
+++ fire.py
@@ -0,0 +1,20 @@
+import cv2
+from playsound import playsound
+
+fire_myys = cv2.CascadeClassifier('fire_detection.xml') #  file name
+myy = cv2.VideoCapture(0)
+while (True):
+    ret,frame = myy.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    fire = fire_myys.detectMultiScale(frame,1.2,5)                         # 1.2 دقه التعرف على النار 
+    # اذا توفرت 5 خواص للنار راح يتعرف عليها 
+    for (x,y,w,h) in fire:
+        roi_gray = gray[y:y+h ,  x:x+w]
+        roi_color = frame[y:y+h ,  x:x+w]
+        print('Fire is detected')
+        playsound('audio.mp3')
+
+    cv2.imshow('MYASSAR', frame)
+    if cv2.waitKey(1) & 0xFF == ord('m') :  # اذا فتحت الكميرا خليها شغاله لحتى اضغط حرف m 
+     break
+    
